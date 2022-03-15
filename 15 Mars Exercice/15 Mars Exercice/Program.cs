b4238b00e24c46dd157aaa3b023197eda838c9fd
grave-tender <carlos.leon@hotmail.fr>
2022-03-15 17:24:12 -0400

    1st commit

---
+++ 15 Mars Exercice/15 Mars Exercice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15_Mars_Exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
