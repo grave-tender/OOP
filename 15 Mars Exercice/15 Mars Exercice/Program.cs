7f6c78c72fb4e3eb8bed64a450c5c1262b1691a1
grave-tender <carlos.leon@hotmail.fr>
2022-03-15 17:51:51 -0400

    Exercice V2

--- 15 Mars Exercice/15 Mars Exercice/Program.cs
+++ 15 Mars Exercice/15 Mars Exercice/Program.cs
@@ -7,9 +7,9 @@ namespace _15_Mars_Exercice
         static void Main(string[] args)
         {
             Console.WriteLine("Entrez les 3 chiffres:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = Convert.ToInt32(Console.ReadLine());
+            int a = readInt();
+            int b = readInt();
+            int c = readInt();
 
             int max = calculateMax(a, b, c);
 
@@ -21,6 +21,20 @@ namespace _15_Mars_Exercice
             if (a > b && a > c) return a;
             if (b > c) return b;
             return c;
+        }
+
+        static int readInt()
+        {
+            int nb = 0;
+            try
+            {
+                nb = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return nb;
         }
     }
 }
