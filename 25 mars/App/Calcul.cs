69958c94725546b74b3e0c1f5570c8c42958f3e8
grave-tender <carlos.leon@hotmail.fr>
2022-03-29 12:31:04 -0400

    29 mars first push

--- 25 mars/App/Calcul.cs
+++ 25 mars/App/Calcul.cs
@@ -4,18 +4,10 @@ namespace App
 {
     class Calcul
     {
-        public int Sum(byte a, byte b){
-            return a + b;
-        }
-        public int Sum(int a, int b){
-            return a + b;
-        }
-        public double Sum(double a, double b){
-            return a + b;
-        }
-        public float Sum(float a, float b){ 
-            return a + b;
-        }
+        public int Sum(byte a, byte b){ return a + b; }
+        public int Sum(int a, int b){ return a + b; }
+        public double Sum(double a, double b){ return a + b; }
+        public float Sum(float a, float b){ return a + b; }
         public int Minus(byte a, byte b){ return a - b; }
         public int Minus(int a, int b){ return a - b; }
         public double Minus(double a, double b){ return a - b; }
